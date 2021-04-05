14df813bfece8003c0648aefa0d85847f030e9cf
Thomas Wagener <thomas.wagener@web.de>
2021-04-05 17:14:45 +0200

    milestone 1 commit

---
+++ src/ticker_data/data_processing.rs
@@ -0,0 +1,156 @@
+use crate::prelude::*;
+
+pub fn max(series: &[f64]) -> Option<f64> {
+    if !series.is_empty() {
+        let max = series
+            .iter()
+            .fold(-INFINITY, |acc, x| if *x > acc { *x } else { acc });
+        return Some(max);
+    }
+    None
+}
+
+pub fn min(series: &[f64]) -> Option<f64> {
+    if !series.is_empty() {
+        let min = series
+            .iter()
+            .fold(INFINITY, |acc, x| if *x < acc { *x } else { acc });
+        return Some(min);
+    }
+    None
+}
+
+pub fn n_window_sma(n: usize, series: &[f64]) -> Option<Vec<f64>> {
+    if series.len() >= n {
+        let result_size = series.len() - n + 1;
+        let mut sma: Vec<f64> = Vec::with_capacity(result_size);
+        for i in 0..result_size {
+            let sum: f64 = series[i..(i + n)].iter().sum();
+            sma.push(sum / n as f64);
+        }
+        return Some(sma);
+    }
+    None
+}
+
+pub fn price_difference(series: &[f64]) -> Option<(f64, f64)> {
+    if series.len() > 1 {
+        let start = series[0];
+        if start > 0.0 {
+            let end = series.last().unwrap();
+            let percentage = end * 100.0 / start;
+            let diff = end - start;
+            return Some((percentage, diff));
+        }
+    }
+    None
+}
+
+pub fn avg(series: &[f64]) -> Option<f64> {
+    if series.len() > 0 {
+        let sum: f64 = series.iter().sum();
+        return Some(sum / (series.len() as f64));
+    }
+    None
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_max_none() {
+        let s: [f64; 0] = [];
+        assert_eq!(max(&s), None);
+    }
+
+    #[test]
+    fn test_max_some() {
+        let s: [f64; 3] = [0.0, 0.1, 10.0];
+        assert_eq!(max(&s), Some(10.0));
+    }
+
+    #[test]
+    fn test_min_none() {
+        let s: [f64; 0] = [];
+        assert_eq!(min(&s), None);
+    }
+
+    #[test]
+    fn test_min_some() {
+        let s: [f64; 3] = [0.0, 0.1, -10.0];
+        assert_eq!(min(&s), Some(-10.0));
+    }
+
+    #[test]
+    fn test_sma_none() {
+        let s: [f64; 3] = [1.0, 2.0, 3.0];
+        assert_eq!(n_window_sma(4, &s), None);
+    }
+
+    #[test]
+    fn test_sma_some_one_value() {
+        let s: [f64; 3] = [1.0, 2.0, 3.0];
+        if let Some(mut result) = n_window_sma(3, &s) {
+            assert_eq!(result.len(), 1);
+            assert_eq!(result.pop(), Some(2.0));
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn test_sma_some_multiple_values() {
+        let s: [f64; 4] = [1.0, 2.0, 3.0, 4.0];
+        if let Some(mut result) = n_window_sma(3, &s) {
+            assert_eq!(result.len(), 2);
+            assert_eq!(result.pop(), Some(3.0));
+            assert_eq!(result.pop(), Some(2.0));
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn test_price_difference_none() {
+        let s: [f64; 0] = [];
+        assert_eq!(price_difference(&s), None);
+        let s: [f64; 1] = [124.3];
+        assert_eq!(price_difference(&s), None);
+    }
+
+    #[test]
+    fn test_price_difference_some() {
+        let s: [f64; 3] = [1.0, 3.0, 2.0];
+        if let Some((perc, diff)) = price_difference(&s) {
+            assert_eq!(perc, 200.0);
+            assert_eq!(diff, 1.0);
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn test_price_difference_zero() {
+        let s: [f64; 3] = [0.0, 3.0, 2.0];
+        assert_eq!(price_difference(&s), None);
+    }
+
+    #[test]
+    fn test_avg_none() {
+        let s: [f64; 0] = [];
+        assert_eq!(avg(&s), None);
+    }
+
+    #[test]
+    fn test_avg_trivial() {
+        let s: [f64; 1] = [1.0];
+        assert_eq!(avg(&s), Some(1.0));
+    }
+
+    #[test]
+    fn test_avg_some() {
+        let s: [f64; 2] = [2.0, 4.0];
+        assert_eq!(avg(&s), Some(3.0));
+    }
+}
---
+++ src/ticker_data/mod.rs
@@ -0,0 +1,175 @@
+mod data_processing;
+mod granularity;
+mod ticker_data;
+
+use std::time::UNIX_EPOCH;
+
+use crate::prelude::*;
+use data_processing::*;
+use granularity::*;
+use std::time::Duration;
+use ticker_data::*;
+use yahoo::{Quote, YResponse, YahooConnector, YahooError};
+
+fn get_quotes(
+    provider: &YahooConnector,
+    ticker: &str,
+    from_date: &DateTime<Utc>,
+    granularity: &Granularity,
+) -> Result<Vec<Quote>, YahooError> {
+    match provider.get_quote_history_interval(
+        ticker,
+        from_date.clone(),
+        Utc::now(),
+        granularity.to_string(),
+    ) {
+        Ok(response) => response.quotes(),
+        Err(error) => Err(error),
+    }
+}
+
+fn get_prices_from_response(response: YResponse) -> Result<Vec<f64>, YahooError> {
+    match response.quotes() {
+        Ok(quotes) => Ok(quotes.iter().map(|quote| quote.adjclose).collect()),
+        Err(yerr) => Err(yerr),
+    }
+}
+
+fn get_prices(
+    provider: &YahooConnector,
+    ticker: &str,
+    from_date: &DateTime<Utc>,
+    granularity: &Granularity,
+) -> Result<Vec<f64>, YahooError> {
+    match get_quotes(provider, ticker, from_date, granularity) {
+        Ok(quotes) => Ok(quotes.iter().map(|quote| quote.adjclose).collect()),
+        Err(yerr) => Err(yerr),
+    }
+}
+
+pub fn get_max_prices<'a>(
+    tickers: &'a mut clap::Values,
+    provider: &'a yahoo::YahooConnector,
+    from_date: &'a DateTime<Utc>,
+) -> HashMap<&'a str, f64> {
+    let granularity = Granularity::Day;
+    let mut result = HashMap::<&str, f64>::new();
+    for ticker in tickers {
+        let quotes = match get_prices(provider, ticker, from_date, &granularity) {
+            Ok(value) => value,
+            Err(_) => {
+                eprintln!("Failed to retrieve quotes for ticker {}", ticker);
+                continue;
+            }
+        };
+        let max_price = max(quotes.as_slice()).unwrap();
+        result.insert(ticker, max_price);
+    }
+    result
+}
+
+pub fn get_min_prices<'a>(
+    tickers: &'a mut clap::Values,
+    provider: &'a yahoo::YahooConnector,
+    from_date: &'a DateTime<Utc>,
+) -> HashMap<&'a str, f64> {
+    let granularity = Granularity::Day;
+    let mut result = HashMap::<&str, f64>::new();
+    for ticker in tickers {
+        let quotes = match get_prices(provider, ticker, from_date, &granularity) {
+            Ok(value) => value,
+            Err(_) => {
+                eprintln!("Failed to retrieve quotes for ticker {}", ticker);
+                continue;
+            }
+        };
+        let min_price = min(quotes.as_slice()).unwrap();
+        result.insert(ticker, min_price);
+    }
+    result
+}
+
+pub fn get_sma_windows<'a>(
+    tickers: &'a mut clap::Values,
+    provider: &'a yahoo::YahooConnector,
+    from_date: &DateTime<Utc>,
+    window: usize,
+) -> HashMap<&'a str, Vec<f64>> {
+    let granularity = Granularity::Day;
+    let mut result = HashMap::new();
+    for ticker in tickers {
+        let quotes = match get_prices(provider, ticker, from_date, &granularity) {
+            Ok(value) => value,
+            Err(_) => {
+                eprintln!("Failed to retrieve quotes for ticker {}", ticker);
+                continue;
+            }
+        };
+        result.insert(ticker, n_window_sma(window, quotes.as_slice()).unwrap());
+    }
+    result
+}
+
+pub fn get_price_differences<'a>(
+    tickers: &'a mut clap::Values,
+    provider: &yahoo::YahooConnector,
+    from_date: &DateTime<Utc>,
+) -> HashMap<&'a str, (f64, f64)> {
+    let granularity = Granularity::Day;
+    let mut result = HashMap::new();
+    for ticker in tickers {
+        let quotes = match get_prices(provider, ticker, from_date, &granularity) {
+            Ok(value) => value,
+            Err(_) => {
+                eprintln!("Failed to retrieve quotes for ticker {}", ticker);
+                continue;
+            }
+        };
+        if let Some(difference) = price_difference(quotes.as_slice()) {
+            result.insert(ticker, difference);
+        } else {
+            eprintln!("Could not calculate difference for {}. Skipping!", ticker);
+        }
+    }
+    result
+}
+
+pub fn get_ticker_summary<'a>(
+    tickers: &'a mut clap::Values,
+    provider: &YahooConnector,
+    from_date: &DateTime<Utc>,
+) -> HashMap<&'a str, TickerData> {
+    let mut result = HashMap::new();
+    let granularity = Granularity::Day;
+    for ticker in tickers {
+        let response = match provider.get_quote_history_interval(
+            ticker,
+            from_date.clone(),
+            Utc::now(),
+            granularity.to_string(),
+        ) {
+            Ok(response) => response,
+            Err(error) => {
+                eprintln!(
+                    "Cannot retrieve response for ticker {}! {:?}",
+                    ticker, error
+                );
+                continue;
+            }
+        };
+
+        let last_quote = response.last_quote().unwrap();
+        let prices = get_prices_from_response(response).unwrap();
+        let mut ticker_data = TickerData::new(ticker);
+        ticker_data.price = last_quote.adjclose;
+        ticker_data.date = DateTime::from(UNIX_EPOCH + Duration::from_secs(last_quote.timestamp));
+        ticker_data.max = max(&prices).unwrap();
+        ticker_data.min = min(&prices).unwrap();
+        ticker_data.avg = avg(&&prices).unwrap();
+        let (perc, diff) = price_difference(&prices).unwrap();
+        ticker_data.perc = perc - 100.0;
+        ticker_data.diff = diff;
+        result.insert(ticker, ticker_data);
+    }
+    result
+}
