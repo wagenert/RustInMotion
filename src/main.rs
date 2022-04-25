b48c7d88d7a52594b3f4ad1f63da8c643a666a7a
Thomas Wagener <thomas.wagener@web.de>
2022-04-25 22:25:45 +0200

    Migrated towards new clap

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ mod ticker_data;
 mod prelude {
     pub use crate::ticker_data::*;
     pub use chrono::{prelude::*, Duration};
-    pub use clap::{App, Arg, SubCommand};
     pub use std::{collections::HashMap, f64::INFINITY, process::exit};
     pub use yahoo_finance_api as yahoo;
 }
 
+use clap::{Command, arg};
 use prelude::*;
+pub use clap::{command};
 
-fn is_valid_date(d: String) -> Result<(), String> {
+fn is_valid_date(d: &str) -> Result<(), String> {
     match NaiveDate::parse_from_str(&d, "%Y-%m-%d") {
         Ok(date) => {
             if Utc.from_utc_date(&date) > Utc::today() {
@@ -35,9 +36,8 @@ fn extract_date(date_str: &str) -> DateTime<Utc> {
     from_date
 }
 
-fn parse_window_param<'a>(sma_matches: Option<&clap::ArgMatches>) -> Result<usize, String> {
-    if let Some(matches) = sma_matches {
-        let result = match matches.value_of("window").unwrap().parse::<usize>() {
+fn parse_window_param<'a>(sma_matches: &clap::ArgMatches) -> Result<usize, String> {
+        let result = match sma_matches.value_of("DAYS").unwrap().parse::<usize>() {
             Ok(value) => Ok(value),
             Err(error) => Err(format!(
                 "Can not parse parameter value of window to a number. {:?}",
@@ -46,68 +46,52 @@ fn parse_window_param<'a>(sma_matches: Option<&clap::ArgMatches>) -> Result<usiz
         };
         return result;
     }
-    Err(String::from("Missing parameter window!"))
-}
 
 fn main() {
-    let matches = App::new("Stock data processor")
-        .version("0.1")
-        .author("Thomas Wagener <thomas.wagener@web.de")
-        .about("Read data for a stock values from a given start date in days")
+    let matches = command!()
+        .propagate_version(true)
+        .subcommand_required(true)
         .subcommand(
-            SubCommand::with_name("max")
-                .about("get maximum closing price in a time period")
-                .version("0.1"),
+            Command::new("max")
+            .about("get maximum closing price in a time period")
         )
         .subcommand(
-            SubCommand::with_name("min")
-                .about("get minimum adjusted closing price in a time period")
-                .version("0.1"),
+            Command::new("min")
+            .about("get minimum adjusted closing price in a time period")
         )
         .subcommand(
-            SubCommand::with_name("sma")
-                .about("get sliding window average for n days in given time period")
-                .version("0.1")
-                .arg(
-                    Arg::with_name("window")
-                        .short("w")
-                        .long("window")
-                        .value_name("DAYS")
-                        .help("size of sliding window in days")
-                        .required(true),
-                ),
+            Command::new("diff")
+            .about("get percentage and absolute price difference for given time period")
         )
         .subcommand(
-            SubCommand::with_name("diff")
-                .about("get percentage and absolut price difference for given time period")
-                .version("0.1"),
+            Command::new("sum")
+            .about("get 30 day summary of tickers")
         )
         .subcommand(
-            SubCommand::with_name("sum")
-                .about("get 30 day summary of tickers")
-                .version("0.1"),
+            Command::new("sma")
+            .about("get sliding window average for n days in given time period")
+            .arg(
+                arg!(-w --window <DAYS>)
+                .help("size of sliding window in days")
+                .required(true)
+                .validator(|d| d.parse::<usize>())
+            )
         )
         .arg(
-            Arg::with_name("ticker")
-                .short("t")
-                .long("ticker")
-                .value_name("SYMBOL")
-                .help("ticker symbol of the stock paper")
-                .value_delimiter(",")
-                .required(true),
+            arg!(-f --from <START_DATE>)
+            .help(
+                "start date from which you want to collect the data. Defaults to last 30 days."
+            )
+            .validator(|d| is_valid_date(d))
         )
         .arg(
-            Arg::with_name("from")
-                .short("f")
-                .long("from")
-                .value_name("START DATE")
-                .help(
-                    "start date from which you want to collect the data. Defaults to last 30 days.",
-                )
-                .validator(is_valid_date),
+            arg!(-t --ticker <SYMBOL>)
+            .help("ticker symbol of the stock paper")
+                .value_delimiter(',')
+                .required(true)
         )
         .get_matches();
-
+        
     let mut tickers = matches.values_of("ticker").unwrap();
     let from_date = match matches.value_of("from") {
         Some(date) => extract_date(date),
@@ -116,21 +100,21 @@ fn main() {
 
     let provider = yahoo::YahooConnector::new();
     match matches.subcommand() {
-        ("max", _) => {
+        Some(("max", _)) => {
             let max_prices = get_max_prices(&mut tickers, &provider, &from_date);
             println!("Max prices:");
             max_prices.iter().for_each(|(key, value)| {
                 println!("{}: {}", *key, value);
             });
         }
-        ("min", _) => {
+        Some(("min", _)) => {
             let min_prices = get_min_prices(&mut tickers, &provider, &from_date);
             println!("Min prices:");
             min_prices.iter().for_each(|(key, value)| {
                 println!("{}: {}", *key, value);
             });
         }
-        ("sma", sma_matches) => {
+        Some(("sma", sma_matches)) => {
             let window: usize = match parse_window_param(sma_matches) {
                 Ok(value) => value,
                 Err(error) => {
@@ -145,7 +129,7 @@ fn main() {
                 println!("{}: {:#?}", *key, values);
             });
         }
-        ("diff", _) => {
+        Some(("diff", _)) => {
             let price_differences: HashMap<&str, (f64, f64)> =
                 get_price_differences(&mut tickers, &provider, &from_date);
             println!("Ticker\tPercent\tDifference");
@@ -153,14 +137,11 @@ fn main() {
                 println!("{}:\t{:.2}%\t{:.2}", *key, perc, diff);
             });
         }
-        ("sum", _) => {
+        Some(("sum", _)) => {
             let ticker_summary = get_ticker_summary(&mut tickers, &provider, &from_date);
             println!("period start,symbol,price,change %,min,max,30d avg");
             ticker_summary.iter().for_each(|(_key, ticker)| println!("{}", ticker));
         }
-        _ => {
-            eprintln!("Unknown command!");
-            exit(-1);
-        }
+        _ => unreachable!("Exhausted list of subcommands. Subcommand required prevents `None`"),
     };
 }
