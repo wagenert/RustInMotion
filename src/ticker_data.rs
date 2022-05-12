d2047e54029657453b60ffcf1b02633f6f2791d3
Thomas Wagener <thomas.wagener@web.de>
2022-05-12 23:03:21 +0200

    Ticker data is compiling now TODO: Fix main

--- src/ticker_data.rs
+++ src/ticker_data.rs
@@ -2,53 +2,58 @@ mod data_processing;
 mod granularity;
 mod ticker_summary;
 
-
-
 use crate::prelude::*;
+use clap::Values;
 use data_processing::*;
 use granularity::*;
 
 use ticker_summary::*;
 use yahoo::{Quote, YahooConnector, YahooError};
 
-fn get_quotes(
-    provider: &YahooConnector,
-    ticker: &str,
+async fn get_quotes(
+    provider: &'static YahooConnector,
+    ticker: &'static str,
     from_date: &DateTime<Utc>,
-    granularity: &Granularity,
+    granularity: &'static Granularity,
 ) -> Result<Vec<Quote>, YahooError> {
-    match provider.get_quote_history_interval(
+    match tokio::spawn(provider.get_quote_history_interval(
         ticker,
         from_date.clone(),
         Utc::now(),
         granularity.to_string(),
-    ) {
+    ))
+    .await
+    .unwrap()
+    {
         Ok(response) => response.quotes(),
         Err(error) => Err(error),
     }
 }
 
-fn get_prices(
-    provider: &YahooConnector,
-    ticker: &str,
-    from_date: &DateTime<Utc>,
-    granularity: &Granularity,
+async fn get_prices(
+    provider: &'static YahooConnector,
+    ticker: &'static str,
+    from_date: &'static DateTime<Utc>,
+    granularity: &'static Granularity,
 ) -> Result<Vec<f64>, YahooError> {
-    match get_quotes(provider, ticker, from_date, granularity) {
+    match tokio::spawn(get_quotes(provider, ticker, from_date, granularity))
+        .await
+        .unwrap()
+    {
         Ok(quotes) => Ok(quotes.iter().map(|quote| quote.adjclose).collect()),
         Err(yerr) => Err(yerr),
     }
 }
 
-pub fn get_max_prices<'a>(
-    tickers: &'a mut clap::Values,
-    provider: &'a yahoo::YahooConnector,
-    from_date: &'a DateTime<Utc>,
-) -> HashMap<&'a str, f64> {
-    let granularity = Granularity::Day;
+pub async fn get_max_prices(
+    tickers: &mut Values<'static>,
+    provider: &'static yahoo::YahooConnector,
+    from_date: &'static DateTime<Utc>,
+) -> HashMap<&'static str, f64> {
+    static granularity: Granularity = Granularity::Day;
     let mut result = HashMap::<&str, f64>::new();
     for ticker in tickers {
-        let quotes = match get_prices(provider, ticker, from_date, &granularity) {
+        let quotes = match tokio::spawn(get_prices(provider, ticker, from_date, &granularity)).await.unwrap() {
             Ok(value) => value,
             Err(_) => {
                 eprintln!("Failed to retrieve quotes for ticker {}", ticker);
@@ -61,15 +66,15 @@ pub fn get_max_prices<'a>(
     result
 }
 
-pub fn get_min_prices<'a>(
-    tickers: &'a mut clap::Values,
-    provider: &'a yahoo::YahooConnector,
-    from_date: &'a DateTime<Utc>,
-) -> HashMap<&'a str, f64> {
-    let granularity = Granularity::Day;
+pub async fn get_min_prices(
+    tickers: &mut Values<'static>,
+    provider: &'static yahoo::YahooConnector,
+    from_date: &'static DateTime<Utc>,
+) -> HashMap<&'static str, f64> {
+    static granularity: Granularity = Granularity::Day;
     let mut result = HashMap::<&str, f64>::new();
     for ticker in tickers {
-        let quotes = match get_prices(provider, ticker, from_date, &granularity) {
+        let quotes = match tokio::spawn(get_prices(provider, ticker, from_date, &granularity)).await.unwrap() {
             Ok(value) => value,
             Err(_) => {
                 eprintln!("Failed to retrieve quotes for ticker {}", ticker);
@@ -82,16 +87,16 @@ pub fn get_min_prices<'a>(
     result
 }
 
-pub fn get_sma_windows<'a>(
-    tickers: &'a mut clap::Values,
-    provider: &'a yahoo::YahooConnector,
-    from_date: &DateTime<Utc>,
+pub async fn get_sma_windows(
+    tickers: &mut Values<'static>,
+    provider: &'static yahoo::YahooConnector,
+    from_date: &'static DateTime<Utc>,
     window: usize,
-) -> Result<HashMap<&'a str, Vec<f64>>, String> {
-    let granularity = Granularity::Day;
+) -> Result<HashMap<&'static str, Vec<f64>>, String> {
+    static granularity: Granularity = Granularity::Day;
     let mut result = HashMap::new();
     for ticker in tickers {
-        let quotes = match get_prices(provider, ticker, from_date, &granularity) {
+        let quotes = match tokio::spawn(get_prices(provider, ticker, from_date, &granularity)).await.unwrap() {
             Ok(value) => value,
             Err(_) => {
                 warn!("Failed to retrieve quotes for ticker {}", ticker);
@@ -107,15 +112,15 @@ pub fn get_sma_windows<'a>(
     Ok(result)
 }
 
-pub fn get_price_differences<'a>(
-    tickers: &'a mut clap::Values,
-    provider: &yahoo::YahooConnector,
-    from_date: &DateTime<Utc>,
-) -> HashMap<&'a str, (f64, f64)> {
-    let granularity = Granularity::Day;
+pub async fn get_price_differences(
+    tickers: &mut Values<'static>,
+    provider: &'static yahoo::YahooConnector,
+    from_date: &'static DateTime<Utc>,
+) -> HashMap<&'static str, (f64, f64)> {
+    static granularity: Granularity = Granularity::Day;
     let mut result = HashMap::new();
     for ticker in tickers {
-        let quotes = match get_prices(provider, ticker, from_date, &granularity) {
+        let quotes = match tokio::spawn(get_prices(provider, ticker, from_date, &granularity)).await.unwrap() {
             Ok(value) => value,
             Err(_) => {
                 eprintln!("Failed to retrieve quotes for ticker {}", ticker);
@@ -131,26 +136,26 @@ pub fn get_price_differences<'a>(
     result
 }
 
-pub fn get_ticker_summary<'a>(
-    tickers: &'a mut clap::Values,
-    provider: &YahooConnector,
+pub async fn get_ticker_summary(
+    tickers: &mut Values<'static>,
+    provider: &'static YahooConnector,
     from_date: &DateTime<Utc>,
-) -> HashMap<&'a str, TickerSummary> {
+) -> HashMap<&'static str, TickerSummary> {
     let mut result = HashMap::new();
-    let granularity = Granularity::Day;
+    static granularity: Granularity = Granularity::Day;
     for ticker in tickers {
-        match provider.get_quote_history_interval(
+        match tokio::spawn(provider.get_quote_history_interval(
             ticker,
             from_date.clone(),
             Utc::now(),
             granularity.to_string(),
-        ) {
+        )).await.unwrap() {
             Ok(response) => match response.quotes() {
                 Ok(quotes) => {
                     let mut ticker_data = TickerSummary::new(ticker);
                     ticker_data.update_ticker_summary(quotes);
                     result.insert(ticker, ticker_data);
-                },
+                }
                 Err(_) => todo!(),
             },
             Err(error) => {
